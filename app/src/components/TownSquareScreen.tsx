d10791330f888f7d34f244302325c90c76414db4
Raven Jiang <raven@cs.stanford.edu>
2020-08-07 21:31:45 -0700

    Minor style

--- app/src/components/TownSquareScreen.tsx
+++ app/src/components/TownSquareScreen.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
+import { createUseStyles } from 'react-jss';
 
 const TownSquareScreen = (
   props: RouteComponentProps<{ squareId?: string }, {}, undefined>
 ) => {
+  const classes = useStyles();
   const squareId = props.match.params['squareId'];
-  return <div>{squareId}</div>;
+  return <div className={classes.TownSquareScreen}>{squareId}</div>;
 };
+
+const useStyles = createUseStyles({
+  TownSquareScreen: {
+    minWidth: 400,
+    minHeight: 400,
+  },
+});
 
 export default TownSquareScreen;
