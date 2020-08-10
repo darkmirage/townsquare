c210ab5217a9ed1b0ffd92b26f305d134c93e5d9
Raven Jiang <raven@cs.stanford.edu>
2020-08-09 23:17:22 -0700

    Docker run Hasura

---
+++ server/docker-run.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# This is for local development purpose only
+docker run -d --net=host -p 8080:8080 \
+       -e HASURA_GRAPHQL_DATABASE_URL=postgres://postgres:thisisfine1@localhost:5432/townsquare \
+       -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
+       -e HASURA_GRAPHQL_DEV_MODE=true \
+       hasura/graphql-engine:v1.3.0
