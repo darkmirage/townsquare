8d85cb254e79dd965155d6879a5447479ddbafdd
Raven Jiang <raven@cs.stanford.edu>
2020-08-25 21:37:00 -0700

    getOrCreateUser action handler

--- server/docker-run.sh
+++ server/docker-run.sh
@@ -5,4 +5,4 @@ docker run -d --net=host \
        -e HASURA_GRAPHQL_DATABASE_URL=postgres://postgres:thisisfine1@localhost:5432/townsquare \
        -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
        -e HASURA_GRAPHQL_DEV_MODE=true \
-       hasura/graphql-engine:v1.3.0
+       hasura/graphql-engine:v1.3.1
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,55 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as admin from 'firebase-admin';
+
+import connectionPromise from './createConnection';
+import User from './entities/User';
+
+const serviceAccount = require('../firebase-service-key.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://townsquare-chat.firebaseio.com',
+});
 
 const app = express();
 const PORT = 3000;
 
+app.use(bodyParser.json());
+
 app.get('/', async (req, res) => {
-  res.send('TownSquare hello Server');
+  res.send('TownSquare Server');
+});
+
+app.post('/graphql/getOrCreateUser', async (req, res) => {
+  const { body } = req;
+  const { firebaseIdToken } = body.input;
+
+  const decodedToken = await admin.auth().verifyIdToken(firebaseIdToken);
+  const firebaseId = decodedToken.uid;
+
+  const connection = await connectionPromise;
+  const { manager } = connection;
+
+  let user = await manager.findOne(User, { firebaseId });
+  if (user) {
+    res.json({ id: user.id });
+    return;
+  }
+
+  user = manager.create(User, {
+    firebaseId,
+    email: decodedToken.email,
+  });
+
+  try {
+    await manager.save(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Unable to create user' });
+    return;
+  }
+
+  res.json({ id: user.id });
 });
 
 app.listen(PORT, () => {
