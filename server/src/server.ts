5693c0e1ea21f81de773be868d90432664f113f3
Raven Jiang <raven@cs.stanford.edu>
2020-08-30 15:56:18 -0700

    Use dotenv in Express server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,11 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as http from 'http';
+import * as dotenv from 'dotenv';
 
 import routes from './routes';
+
+dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
