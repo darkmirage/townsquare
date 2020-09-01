50d279bd372ead85cdf2a02c30dcf0acb072db2f
Raven Jiang <raven@cs.stanford.edu>
2020-09-01 09:21:58 -0700

    Close audio track after disconnecting

--- app/src/agoraClient.ts
+++ app/src/agoraClient.ts
@@ -1,4 +1,4 @@
-import AgoraRTC from 'agora-rtc-sdk-ng';
+import AgoraRTC, { IMicrophoneAudioTrack } from 'agora-rtc-sdk-ng';
 
 import { AGORA_APP_ID } from 'config';
 
@@ -20,10 +20,17 @@ export async function joinChannel(channel: string, token: string, uid: string) {
   await client.join(AGORA_APP_ID, channel, token, uid);
 }
 
+let localAudioTrack: IMicrophoneAudioTrack | null = null;
+
 client.on('connection-state-change', async () => {
   if (client.connectionState === 'CONNECTED') {
-    const localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
+    localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
     await client.publish([localAudioTrack]);
+  } else if (client.connectionState === 'DISCONNECTED') {
+    if (localAudioTrack) {
+      localAudioTrack.close();
+      localAudioTrack = null;
+    }
   }
 });
 
